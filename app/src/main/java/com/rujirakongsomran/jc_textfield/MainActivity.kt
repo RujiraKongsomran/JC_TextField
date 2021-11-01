788e4a4f4222716ada88163c967fe9faec5a9b5c
RujiraKongsomran <rujirabiwberry@gmail.com>
2021-11-01 22:20:18 +0700

    finish

--- app/src/main/java/com/rujirakongsomran/jc_textfield/MainActivity.kt
+++ app/src/main/java/com/rujirakongsomran/jc_textfield/MainActivity.kt
@@ -3,11 +3,18 @@ package com.rujirakongsomran.jc_textfield
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.rujirakongsomran.jc_textfield.ui.theme.JC_TextFieldTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,11 +24,65 @@ class MainActivity : ComponentActivity() {
             JC_TextFieldTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+                    PasswordTextField()
                 }
             }
         }
     }
+}
+
+@Composable
+fun SimpleFilledTextFieldSample() {
+    var text by remember { mutableStateOf("Hello") }
+
+    TextField(
+        value = text,
+        onValueChange = { text = it },
+        label = { Text(text = "Label") }
+    )
+}
+
+@Composable
+fun SimpleOutlinedTextFieldSample() {
+    var text by remember {
+        mutableStateOf("")
+    }
+
+    OutlinedTextField(
+        value = text,
+        onValueChange = { text = it },
+        label = { Text(text = "Label") }
+    )
+}
+
+@Composable
+fun StyledTextField() {
+    var value by remember {
+        mutableStateOf("Hello\nWorld\nInvisible")
+    }
+    TextField(
+        value = value,
+        onValueChange = { value = it },
+        label = { Text("Enter text") },
+        maxLines = 3,
+        textStyle = TextStyle(color = Color.Blue, fontWeight = FontWeight.Bold),
+        modifier = Modifier.padding(20.dp)
+    )
+}
+
+@Composable
+fun PasswordTextField() {
+    var password by remember {
+        mutableStateOf("")
+    }
+
+    TextField(
+        value = password,
+        onValueChange = { password = it },
+        label = { Text("Enter password") },
+        visualTransformation = PasswordVisualTransformation(),
+        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password)
+    )
 }
 
 @Composable
@@ -33,6 +94,6 @@ fun Greeting(name: String) {
 @Composable
 fun DefaultPreview() {
     JC_TextFieldTheme {
-        Greeting("Android")
+        PasswordTextField()
     }
 }
